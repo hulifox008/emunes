ac359e86b9a87624f43544fe20b273fe11c788d5
fox <fox@localhost.(none)>
2011-11-20 20:31:22 -0600

    ....

--- cpu.c
+++ cpu.c
@@ -20,16 +20,26 @@ void unimp(struct cpu6502 *cpu, u_int8_t instruction)
 instruction_impl(0x78, SEI)
 {
     if(cpu->disasm)
-        printf("%04X: %02X SEI\n", cpu->pc, instruction);
+        printf("%04X: %02X              SEI\n", cpu->pc, instruction);
     cpu->p |= PF_INT;
     cpu->pc++;
     cpu->cycles += 2;
 }
 
+instruction_impl(0x4c, JMP_abs)
+{
+    u_int8_t l = bus_readb(cpu->bus, cpu->pc+1);
+    u_int8_t h = bus_readb(cpu->bus, cpu->pc+2);
+    if(cpu->disasm)
+        printf("%04X: %02X %02X %02X   JMP $%02X%02X\n", cpu->pc, instruction, l, h, h, l);
+    cpu->pc = (h<<8)+l;
+    cpu->cycles += 3;
+}
+
 instruction_impl(0xd8, CLV)
 {
     if(cpu->disasm)
-        printf("%04X: %02X CLV\n", cpu->pc, instruction);
+        printf("%04X: %02X              CLV\n", cpu->pc, instruction);
     cpu->p &= PF_OVERFLOW;
     cpu->pc++;
     cpu->cycles += 2;
@@ -41,7 +51,7 @@ instruction_impl(0xee, INC_abs)
     u_int8_t h = bus_readb(cpu->bus, cpu->pc+2);
     u_int8_t v;
     if(cpu->disasm)
-        printf("%04X: %02X INC $%02X%02X\n", cpu->pc, instruction, h, l);
+        printf("%04X: %02X %02X %02X    INC $%02X%02X\n", cpu->pc, instruction, l, h, h, l);
 
     v = bus_readb(cpu->bus, l|h<<8)+1;
     bus_writeb(cpu->bus, l|h<<8, v);
@@ -66,7 +76,7 @@ unimp, unimp, unimp, unimp, unimp, unimp, unimp, unimp,
 unimp, unimp, unimp, unimp, unimp, unimp, unimp, unimp, 
 /*0x40 - 0x4F*/
 unimp, unimp, unimp, unimp, unimp, unimp, unimp, unimp, 
-unimp, unimp, unimp, unimp, unimp, unimp, unimp, unimp, 
+unimp, unimp, unimp, unimp, JMP_abs, unimp, unimp, unimp, 
 /*0x50 - 0x5F*/
 unimp, unimp, unimp, unimp, unimp, unimp, unimp, unimp, 
 unimp, unimp, unimp, unimp, unimp, unimp, unimp, unimp, 
@@ -105,7 +115,7 @@ unimp, unimp, unimp, unimp, unimp, unimp, unimp, unimp,
 struct cpu6502 *cpu_new()
 {   
     struct cpu6502 *cpu = (struct cpu6502 *)malloc(sizeof(struct cpu6502));
-    cpu->disasm = 0;
+    cpu->disasm = 1;
     return cpu;
 }
 
